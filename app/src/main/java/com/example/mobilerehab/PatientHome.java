027f8a2a6b1eeb52dfef0359bd0ef1a2e03f0e83
MuhdSyahmi <muhamad.syahmi.syazwan@gmail.com>
2019-04-21 02:59:01 +0800

    Add maps activity, get nearby places data

--- app/src/main/java/com/example/mobilerehab/PatientHome.java
+++ app/src/main/java/com/example/mobilerehab/PatientHome.java
@@ -1,13 +1,27 @@
 package com.example.mobilerehab;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PatientHome extends AppCompatActivity {
+
+    Button button_patientviewlocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_home);
+
+        button_patientviewlocation = (Button) findViewById(R.id.button_patientviewlocation);
+        button_patientviewlocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),MapsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
